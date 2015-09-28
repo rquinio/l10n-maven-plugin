8bd3fc4afe8f557cb5b4604a43f2370315c676b8
romain.quinio <romain.quinio@gmail.com>
2015-09-28 22:54:30 +0200

    Add staging to multi-module report it #16

--- src/it/report-multi-module/verify.groovy
+++ src/it/report-multi-module/verify.groovy
@@ -2,3 +2,5 @@ report = new File(basedir, "moduleA/target/site/l10n-report.html");
 assert report.size() > 0;
 report = new File(basedir, "moduleB/target/site/l10n-report.html");
 assert report.size() > 0;
+report = new File(basedir, "target/site/index.html");
+assert report.size() > 0;
